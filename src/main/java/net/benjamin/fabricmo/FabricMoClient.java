2a64163f79bb7e0ac92ba623a5073bce9450dc55
Mew-10 <104922242+Mew-10@users.noreply.github.com>
2022-06-13 19:10:29 +0100

    Initial commit

---
+++ src/main/java/net/benjamin/fabricmo/FabricMoClient.java
@@ -0,0 +1,10 @@
+package net.benjamin.fabricmo;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FabricMoClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
